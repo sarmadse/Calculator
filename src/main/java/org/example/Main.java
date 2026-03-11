afa5bb52027114f7a73d7ceb09ebae54409fd2c0
Sarmad Munir <sarmad.munir@sigmatechnology.com>
2026-03-11 14:27:20 +0100

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(10, 5));
+        System.out.println("Subtract: " + calc.subtract(10, 5));
+        System.out.println("Multiply: " + calc.multiply(10, 5));
+        System.out.println("Divide: " + calc.divide(10, 5));
+    }
+}
